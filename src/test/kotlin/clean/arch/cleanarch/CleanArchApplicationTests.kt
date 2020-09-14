0666fd6cb27c5dc4f94a9484bf8cb98059a273c5
bressani <jr.bressani@gmail.com>
2020-09-14 15:11:24 -0300

    Adds project structure

---
+++ src/test/kotlin/clean/arch/cleanarch/CleanArchApplicationTests.kt
@@ -0,0 +1,13 @@
+package clean.arch.cleanarch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
