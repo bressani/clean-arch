3694824ea8f7352311ce018178975bf4d6c4aa0f
bressani <jr.bressani@gmail.com>
2020-09-28 13:47:30 -0300

    Remove unused fixture

--- src/test/kotlin/cleanarch/fixtures/UserDomainFixture.kt
+++ src/test/kotlin/cleanarch/fixtures/UserDomainFixture.kt
@@ -4,14 +4,6 @@ import cleanarch.domain.entities.UserDomain
 import java.util.UUID
 
 fun getRegularUserDomain(
-    name: String = "Kakaroto"
-): UserDomain {
-    return UserDomain(
-        name = name
-    )
-}
-
-fun getRegularUserDomain(
     id: String? = "185840a0",
     uuid: UUID? = UUID.fromString("185840a0-c51a-4996-b30e-73f0c7f8eca0"),
     name: String = "Kakaroto"
